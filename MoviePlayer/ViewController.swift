c47e44a42f3c7b35d90214651dcc39c319e15a8d
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2023-10-27 12:51:45 +0900

    Initial Commit

---
+++ MoviePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by yeon I on 10/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
